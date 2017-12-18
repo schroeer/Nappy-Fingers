96d084cfd7adb99a660aeb85386d41e8582d5537
schroeer <schroeer@9472904b-5c12-0410-92ad-c346ed384f73>
2017-12-18 13:59:29 +0000

    wip

--- trunk/Trainer/test1.js
+++ trunk/Trainer/test1.js
@@ -1,31 +1,3 @@
-function Set(parms) {
-    this.title = parms.title;
-    this.left  = parms.left;
-    this.right = parms.right;
-    this.hold  = parms.hold;
-    this.break = parms.break;
-    this.reps  = parms.reps;
-    
-    this.duration = function() {
-        return this.hold * this.reps + this.break * (this.reps - 1);
-    };
-
-    this.hang_time = function() {
-        return this.hold * this.reps;
-    };
-}
-
-var test_set = {
-    "title": "Hang on straight arms",
-    "left":  1,
-    "right": 1,
-    "hold":  7,
-    "break": 3,
-    "reps":  5,
-};
-
-var ss = new Set(test_set);
-
 var inputForm = document.querySelector('form');
 var inputTxt = document.querySelector('.txt');
 
