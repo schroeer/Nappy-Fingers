3a5a8b4945d119ed1fe0299f10566fed931460c4
schroeer <schroeer@9472904b-5c12-0410-92ad-c346ed384f73>
2017-12-18 17:14:20 +0000

    wip

--- trunk/Trainer/callbacks.js
+++ trunk/Trainer/callbacks.js
@@ -5,6 +5,8 @@ function Set(parms) {
     this.hold  = parms.hold;
     this.break = parms.break;
     this.reps  = parms.reps;
+    
+    this.r     = 0;
 
     this.next  = (function() {
         var round = 0;
@@ -27,6 +29,37 @@ Set.prototype.hang_time = function() {
     return this.hold * this.reps;
 };
 
+Set.prototype.finished = function() {
+    return this.r >= this.reps;
+};
+
+Set.prototype.next_round = function() {
+    this.r++;
+    return this;
+};
+
+function Countdown(steps, interval, start_func, inter_func, after_func) {
+    this.steps      = steps;
+    this.interval   = interval;
+    this.start_func = start_func;
+    this.inter_func = inter_func;
+    this.after_func = after_func;
+}
+
+Countdown.prototype.start = function() {
+    var timer = window.setInterval(step, this.interval);
+    var step = 0;
+    this.start_func();
+    
+    function step() {
+        this.inter_func(++step);
+        if (step == this.steps) {
+            window.clearInterval(timer);
+            after_func();
+        }
+    }
+}
+
 var test_set = {
     "title": "Hang on straight arms",
     "left":  1,
@@ -38,3 +71,38 @@ var test_set = {
 
 var ss = new Set(test_set);
 
+function doRep(set, next) {
+    if (set.finished()) {
+        next();
+    }
+    else {
+        console.log(`doing repetition ${set.r}`);
+        var timeoutID = window.setTimeout(
+            function() {
+                console.log(`done with repetition ${set.r}`);
+                doRep(set.next_round(), next);
+            },
+            500
+        );
+    }
+}
+
+function finish() {
+    console.log("done");
+}
+
+//doRep(ss, finish);
+
+var cd = new Countdown(
+    3,
+    500,
+    function() {
+        console.log("starte countdown");
+    },
+    function(step) {
+        console.log("step " + step);
+    },
+    finish
+);
+
+cd.start();
