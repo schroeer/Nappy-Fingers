3c1315e8f95ab8bf528311b0153300460d150224
schroeer <schroeer@9472904b-5c12-0410-92ad-c346ed384f73>
2017-12-21 15:21:17 +0000

    WIP

--- basic_training.js
+++ basic_training.js
@@ -7,3 +7,153 @@ var test_set = {
     "reps":  3,
 };
 
+var training1 = {
+    "title":        "Example training",
+    "description":  "A simple example training with two sets of exercises",
+    "board":        "bm1000",
+    "sets":         [
+        {
+            "title":        "Warm up",
+            "description":  "Hang on straight arms",
+            "left":         1,
+            "right":        1,
+            "hold":         3,
+            "break":        2,
+            "reps":         3,
+            "pause":        20,
+        },
+        {
+            "title":        "Hang",
+            "description":  "Hang on straight arms",
+            "left":         2,
+            "right":        2,
+            "hold":         7,
+            "break":        3,
+            "reps":         3,
+            "pause":        20,
+        }
+    ]
+};
+
+var board1 = {
+    "id":    "bm1000",
+    "name":  "Beastmaker 1000",
+    "image": "beastmaker1000.jpg",
+    "holds": [
+        {
+            "id":   1,
+            "name": "jug",
+            "image": "bm1000_01.jpg"
+        },
+        {
+            "id":   2,
+            "name": "hard sloper",
+            "image": "bm1000_02.jpg"
+        },
+        {
+            "id":   3,
+            "name": "easy sloper",
+            "image": "bm1000_03.jpg"
+        },
+        {
+            "id":   4,
+            "name": "hard four finger pocket",
+            "image": "bm1000_04.jpg"
+        },
+        {
+            "id":   5,
+            "name": "easy four finger pocket",
+            "image": "bm1000_05.jpg"
+        },
+        {
+            "id":   6,
+            "name": "medium four finger pocket",
+            "image": "bm1000_06.jpg"
+        },
+        {
+            "id":   7,
+            "name": "easy two finger pocket",
+            "image": "bm1000_07.jpg"
+        },
+        {
+            "id":   8,
+            "name": "hard two finger pocket",
+            "image": "bm1000_08.jpg"
+        },
+        {
+            "id":   9,
+            "name": "easy three finger pocket",
+            "image": "bm1000_09.jpg"
+        },
+        {
+            "id":   10,
+            "name": "hard three finger pocket",
+            "image": "bm1000_10.jpg"
+        },
+        {
+            "id":   11,
+            "name": "medium three finger pocket",
+            "image": "bm1000_11.jpg"
+        },
+        {
+            "id":   21,
+            "name": "jug",
+            "image": "bm1000_21.jpg"
+        },
+        {
+            "id":   22,
+            "name": "hard sloper",
+            "image": "bm1000_22.jpg"
+        },
+        {
+            "id":   23,
+            "name": "easy sloper",
+            "image": "bm1000_23.jpg"
+        },
+        {
+            "id":   24,
+            "name": "hard four finger pocket",
+            "image": "bm1000_24.jpg"
+        },
+        {
+            "id":   25,
+            "name": "easy four finger pocket",
+            "image": "bm1000_25.jpg"
+        },
+        {
+            "id":   26,
+            "name": "medium four finger pocket",
+            "image": "bm1000_26.jpg"
+        },
+        {
+            "id":   27,
+            "name": "easy two finger pocket",
+            "image": "bm1000_27.jpg"
+        },
+        {
+            "id":   28,
+            "name": "hard two finger pocket",
+            "image": "bm1000_28.jpg"
+        },
+        {
+            "id":   29,
+            "name": "easy three finger pocket",
+            "image": "bm1000_29.jpg"
+        },
+        {
+            "id":   30,
+            "name": "hard three finger pocket",
+            "image": "bm1000_30.jpg"
+        },
+        {
+            "id":   31,
+            "name": "medium three finger pocket",
+            "image": "bm1000_31.jpg"
+        },
+        {
+            "id":   40,
+            "name": "central pocket",
+            "image": "bm1000_40.jpg"
+        }
+    ]
+};
