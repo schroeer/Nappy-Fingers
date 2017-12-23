919890b95d41105349aacb57bc5e3fb91668a461
schroeer <schroeer@9472904b-5c12-0410-92ad-c346ed384f73>
2017-12-23 21:46:38 +0000

    WIP

--- trunk/Trainer/trainer.js
+++ trunk/Trainer/trainer.js
@@ -1,5 +1,39 @@
-var makeCounter = function (steps, interval, cb) {
-    var count, timer, resolve, reject;
+var counter_div = document.getElementById("counter");
+var hold_pbar = document.getElementById("hold_pbar");
+var break_pbar = document.getElementById("break_pbar");
+
+var set_title_div = document.getElementById("set_title");
+var set_description_div = document.getElementById("set_description");
+
+var overlay_left_img = document.getElementById("overlay_left");
+var overlay_right_img = document.getElementById("overlay_right");
+
+var pause_button = document.getElementsByName("pause")[0];
+
+var board = board1; // muss gesucht werden
+
+var synth = window.speechSynthesis;
+var voice;
+
+var utter_go = new SpeechSynthesisUtterance("Go!");
+//        utter_go.voice = voice;
+utter_go.lang = 'en-US';
+
+function selectVoice() {
+    var voices = synth.getVoices();
+    for (var i = 0; i < voices.length ; i++) {
+        if (voices[i].lang.startsWith('en')) {
+            voice = voices[i];
+            break;
+        }
+    }
+    if (voice) {
+        console.log('selected voice: ' + voice.name);
+    }
+}
+
+var counter = (function () {
+    var count, timer, paused, resolve, reject, steps, interval, cb;
     
     function step() {
         cb(++count);
@@ -10,8 +44,12 @@ var makeCounter = function (steps, interval, cb) {
     }
     
     return {
-        start: function() {
+        start: function(_steps, _interval, _cb) {
+            steps = _steps;
+            interval = _interval;
+            cb = _cb;
             count = 0;
+            paused = false;
             return new Promise(function(_resolve, _reject) {
                 resolve = _resolve;
                 reject = _reject;
@@ -24,49 +62,36 @@ var makeCounter = function (steps, interval, cb) {
             reject("counter aborted");
         },
         pause: function() {
-            window.clearInterval(timer);
-        },
-        resume: function() {
-            timer = window.setInterval(step, interval);
+            if (paused) {
+                timer = window.setInterval(step, interval);
+                paused = false;
+            }
+            else {
+                window.clearInterval(timer);
+                paused = true;
+            }
         }
     }
-}
+})();
 
-var counter_div = document.getElementById("counter");
-var hold_pbar = document.getElementById("hold_pbar");
-var break_pbar = document.getElementById("break_pbar");
-
-var set_title_div = document.getElementById("set_title");
-var set_description_div = document.getElementById("set_description");
-
-var overlay_left_img = document.getElementById("overlay_left");
-var overlay_right_img = document.getElementById("overlay_right");
-
-var board = board1; // muss gesucht werden
+pause_button.addEventListener("click", counter.pause);
 
 async function runSet(set) {
-    var hold_counter = makeCounter(
-        set.hold,
-        1000,
-        function(step) {
-            counter_div.textContent = set.hold - step;
-            hold_pbar.value = step;
-        }
-    );
-    var break_counter = makeCounter(
-        set.break,
-        1000,
-        function(step) {
-            counter_div.textContent = set.break - step;
-            break_pbar.value = step;
-        }
-    );
     
+    var utter_desc = new SpeechSynthesisUtterance(set.description + " for " + set.hold + " seconds. " + "Left hand " + board.holds[set.left].name + ". Right hand " + board.holds[set.right].name + ". Repeat " + set.reps + " times.");
+    //        utter_desc.voice = voice;
+    utter_desc.lang = 'en-US';
+    synth.speak(utter_desc);
+
     set_title_div.textContent = set.title;
     set_description_div.textContent = set.description;
     
     overlay_left_img.src = "images/" + board.holds[set.left].image;
     overlay_right_img.src = "images/" + board.holds[set.right].image;
+    
+    pause_pbar.style.display = "none";
+    hold_pbar.style.display = "inline-block";
+    break_pbar.style.display = "inline-block";
     
     hold_pbar.max = set.hold;
     hold_pbar.style.width = 100 * set.hold / (set.hold + set.break) + "%";
@@ -79,15 +104,28 @@ async function runSet(set) {
         break_pbar.value = 0;
         
         console.log(`rep ${rep+1}: hold`);
-        await hold_counter.start();
+        synth.speak(utter_go);
+        await counter.start(
+            set.hold,
+            1000,
+            function(step) {
+                counter_div.textContent = set.hold - step;
+                hold_pbar.value = step;
+            }
+        );
 
         console.log(`rep ${rep+1}: break`);
-        await break_counter.start();
+        await counter.start(
+            set.break,
+            1000,
+            function(step) {
+                counter_div.textContent = set.break - step;
+                break_pbar.value = step;
+            }
+        );
     }
     
     console.log("done");
-
-//    console.error(err);
 }
 
 async function runTraining(training) {
@@ -97,7 +135,12 @@ async function runTraining(training) {
         
         if (training.sets[i].pause > 0) {
             pause_pbar.max = training.sets[i].pause;
-            var pause_counter = makeCounter(
+            pause_pbar.style.display = "inline-block";
+            hold_pbar.style.display = "none";
+            break_pbar.style.display = "none";
+
+            console.log(`pause ${training.sets[i].pause} seconds`);
+            await counter.start(
                 training.sets[i].pause,
                 1000,
                 function(step) {
@@ -105,10 +148,13 @@ async function runTraining(training) {
                     pause_pbar.value = step;
                 }
             );
-            console.log(`pause ${training.sets[i].pause} seconds`);
-            await pause_counter.start();
         }
     }
 }
 
-//runTraining(training1);
+selectVoice();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = selectVoice;
+}
+
+runTraining(training1);
