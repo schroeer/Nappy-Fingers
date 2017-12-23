4726e527a97e9211e660b05da53a862ff317f143
schroeer <schroeer@9472904b-5c12-0410-92ad-c346ed384f73>
2017-12-23 16:49:12 +0000

    WIP

--- trunk/Trainer/test1.js
+++ trunk/Trainer/test1.js
@@ -4,32 +4,74 @@ var inputTxt = document.querySelector('.txt');
 var synth = window.speechSynthesis;
 var voice;
 
+
+function Set(parms) {
+    this.title = parms.title;
+    this.left  = parms.left;
+    this.right = parms.right;
+    this.hold  = parms.hold;
+    this.break = parms.break;
+    this.reps  = parms.reps;
+    
+    this.duration = function() {
+        return this.hold * this.reps + this.break * (this.reps - 1);
+    };
+
+    this.hang_time = function() {
+        return this.hold * this.reps;
+    };
+}
+
 function selectVoice() {
     var voices = synth.getVoices();
+    var list = document.getElementById('voicelist');
     for (var i = 0; i < voices.length ; i++) {
-        if (voices[i].lang.startsWith("en")) {
+        var el = document.createElement("li");
+        el.append(voices[i].name + ' (default: ' + voices[i].default + '; local: ' + voices[i].localService + ')');
+        list.append(el);
+    }
+    for (var i = 0; i < voices.length ; i++) {
+        if (voices[i].lang.startsWith('en')) {
             voice = voices[i];
             break;
         }
     }
-}
-selectVoice();
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = selectVoice;
+    if (voice) {
+        document.getElementById('info').append('selected voice: ' + voice.name);
+    }
 }
 
 function speak(){
-  if(inputTxt.value !== ''){
-    var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-    utterThis.voice = voice;
-    synth.speak(utterThis);
-  }
+    if(inputTxt.value !== ''){
+        var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+//        utterThis.voice = voice;
+        utterThis.lang = 'en-US';
+        synth.speak(utterThis);
+    }
 }
 
-inputForm.onsubmit = function(event) {
-  event.preventDefault();
+(function() {
+    var test_set = {
+        "title": "Hang on straight arms",
+        "left":  1,
+        "right": 1,
+        "hold":  7,
+        "break": 3,
+        "reps":  5,
+    };
 
-  speak();
+    var ss = new Set(test_set);
 
-  inputTxt.blur();
-}
+    selectVoice();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+      speechSynthesis.onvoiceschanged = selectVoice;
+    }
+
+    inputForm.onsubmit = function(event) {
+      event.preventDefault();
+
+      speak();
+
+      inputTxt.blur();
+    }
+})();
